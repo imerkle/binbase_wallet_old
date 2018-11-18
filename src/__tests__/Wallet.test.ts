6bf94d026cd0d3c9edda4f33a152561b11e274ed
slim <dsslimshaddy@gmail.com>
2018-11-18 23:47:50 +0530

    add more coins test

--- src/__tests__/Wallet.test.ts
+++ src/__tests__/Wallet.test.ts
@@ -2,10 +2,35 @@
 const mnemonic = "connect ritual news sand rapid scale behind swamp damp brief explain ankle";
 import OmniJs from '../app/omnijs/omnijs'
 
-describe('Wallet Tests', function () {
+describe('Wallet Address Generation', function () {
     it('Bitcoin', function () {
         const omni = new OmniJs('BTC', 'BTC');
         const k = omni.generateSeed(mnemonic);
         expect(k.address).toEqual(`1JN2GamM8pXmJvSRKxiRBppf9Zgur6Ze7L`);
+    })
+    it('Litecoin', function () {
+        const omni = new OmniJs('LTC', 'BTC');
+        const k = omni.generateSeed(mnemonic);
+        expect(k.address).toEqual(`LfPqHoYgghwxjJt2BrjrqwPjhRwk69VFZx`);
+    })
+    it('Dash', function () {
+        const omni = new OmniJs('LTC', 'BTC');
+        const k = omni.generateSeed(mnemonic);
+        expect(k.address).toEqual(`XyQ5Dc9abDeRpiihst9JY1M5fhTv4nKJL5`);
+    })
+    it('Ethereum', function () {
+        const omni = new OmniJs('ETH', 'ETH');
+        const k = omni.generateSeed(mnemonic);
+        expect(k.address).toEqual(`0xb023b80afad0363ab966cf10b5f76E5f625Cf497`);
+    })
+    it('Vechain', function () {
+        const omni = new OmniJs('VET', 'VET');
+        const k = omni.generateSeed(mnemonic);
+        expect(k.address).toEqual(`0xb023b80afad0363ab966cf10b5f76E5f625Cf497`);
+    })
+    it('Ripple', function () {
+        const omni = new OmniJs('XRP', 'XRP');
+        const k = omni.generateSeed(mnemonic);
+        expect(k.address).toEqual(`rPphbLGemSQv4De1LUHYq6tupBkrrZUxNe`);
     })
 });
