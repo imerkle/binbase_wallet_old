f1ad2aff6fe2659f3d51a14882590f0286cff795
slim <dsslimshaddy@gmail.com>
2018-11-21 21:38:21 +0530

    remoe xhr

--- src/app/stores/createStore.ts
+++ src/app/stores/createStore.ts
@@ -3,7 +3,6 @@ import { RouterStore } from './RouterStore';
 import { AppStore } from './AppStore';
 import { LangStore } from './LangStore';
 import { LoginStore } from './LoginStore';
-import { XhrStore } from './XhrStore';
 import { ExchangeStore } from './ExchangeStore';
 import { PriceStore } from './PriceStore';
 import { CoinStore } from './CoinStore';
@@ -13,7 +12,6 @@ export function createStores(history: History) {
   const appStore = new AppStore();
   const langStore = new LangStore();
   const loginStore = new LoginStore();
-  const xhrStore = new XhrStore();
   const coinStore = new CoinStore();
   const exchangeStore = new ExchangeStore(coinStore);
   const priceStore = new PriceStore();
@@ -23,7 +21,6 @@ export function createStores(history: History) {
     appStore,
     langStore,
     loginStore,
-    xhrStore,
     exchangeStore,
     priceStore,
     coinStore,
