01f7fa0795a0807361722aac14d872bd1208eaa5
slim <dsslimshaddy@gmail.com>
2018-11-24 00:21:15 +0530

    unusedstores remove

--- src/app/stores/createStore.ts
+++ src/app/stores/createStore.ts
@@ -1,7 +1,6 @@
 import { History } from 'history';
 import { RouterStore } from './RouterStore';
 import { AppStore } from './AppStore';
-import { LangStore } from './LangStore';
 import { LoginStore } from './LoginStore';
 import { ExchangeStore } from './ExchangeStore';
 import { PriceStore } from './PriceStore';
@@ -10,7 +9,6 @@ import { CoinStore } from './CoinStore';
 export function createStores(history: History) {
   const routerStore = new RouterStore(history);
   const appStore = new AppStore();
-  const langStore = new LangStore();
   const loginStore = new LoginStore();
   const coinStore = new CoinStore();
   const exchangeStore = new ExchangeStore(coinStore);
@@ -19,7 +17,6 @@ export function createStores(history: History) {
   return {
     routerStore,
     appStore,
-    langStore,
     loginStore,
     exchangeStore,
     priceStore,
