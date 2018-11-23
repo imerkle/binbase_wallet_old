0d667593ee65e8d379eb257173f7b2be63e61c95
slim <dsslimshaddy@gmail.com>
2018-11-24 00:40:56 +0530

    remove Loginstore

--- src/app/stores/createStore.ts
+++ src/app/stores/createStore.ts
@@ -1,7 +1,6 @@
 import { History } from 'history';
 import { RouterStore } from './RouterStore';
 import { AppStore } from './AppStore';
-import { LoginStore } from './LoginStore';
 import { ExchangeStore } from './ExchangeStore';
 import { PriceStore } from './PriceStore';
 import { CoinStore } from './CoinStore';
@@ -9,7 +8,6 @@ import { CoinStore } from './CoinStore';
 export function createStores(history: History) {
   const routerStore = new RouterStore(history);
   const appStore = new AppStore();
-  const loginStore = new LoginStore();
   const coinStore = new CoinStore();
   const exchangeStore = new ExchangeStore(coinStore);
   const priceStore = new PriceStore();
@@ -17,7 +15,6 @@ export function createStores(history: History) {
   return {
     routerStore,
     appStore,
-    loginStore,
     exchangeStore,
     priceStore,
     coinStore,
