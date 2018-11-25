4f5831b857272e08e6e86decee9f4dad90188058
slim <dsslimshaddy@gmail.com>
2018-11-25 13:54:28 +0530

    shared_stores

--- src/app/components/TabContainer/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React  from 'react';
-import { Div } from 'app/components';
-
-const TabContainer = (props) => 
-  (<Div style={{minHeight: props.swipeableHeight}} className={props.className}>
-    {props.children}
-  </Div>)
-
-export default TabContainer;
